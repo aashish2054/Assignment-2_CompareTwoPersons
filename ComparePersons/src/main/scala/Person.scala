5d740f99e82e8610d715466794b7d29b60ada79d
aashish2054 <ashish.chaudhary@knoldus.com>
2021-03-16 03:25:22 +0530

    Assignment submission by Ashish

---
+++ ComparePersons/src/main/scala/Person.scala
@@ -0,0 +1,13 @@
+class Person(val name: String, val age: Int) extends Ordered[Person] {
+  def compare(that: Person): Int = {
+    if((this.name compare that.name)== 0 ) {
+      this.age - that.age
+    } else {
+      this.name.length - that.name.length
+    }
+  }
+}
+
+
+
+
